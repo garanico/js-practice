cff8551a230ba72f229acb2a2a84d99ff4149269
Grace Aranico <grace.aranico@gmail.com>
2021-07-08 21:22:33 -0700

    start of Day4

---
+++ Day4/js/main.js
@@ -0,0 +1,28 @@
+//Logic
+
+// if a
+if(true){
+    console.log('a: this code runs if true')
+}
+
+// if / else b
+if(true){
+    console.log('this code runs if true')
+}
+else{
+    console.log('b: this runs if false')
+}
+
+//if /else if / else
+if (false){
+    console.log('c: this runs if true')
+}
+else if (true){
+    console.log('c: this is the second condition')
+}
+else if (true){
+    console.log('c: this is the third condition')
+ }
+ else {
+     console.log('c: this runs if false')
+ }
