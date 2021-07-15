d73c93ceeb88361ec9dcd8bb024c791d95184b1b
Grace Aranico <grace.aranico@gmail.com>
2021-07-15 00:47:49 -0700

    updated notes

--- Day-5/js/d5-takeHome.js
+++ Day-5/js/d5-takeHome.js
@@ -9,59 +9,33 @@
 
 let password = prompt("Please enter a password that is 6-20 charaters long.  Password must start with a letter."); //ask the user to input a password with required parameters.
 
-//console.log(typeof (password)); //checks the data type of the input
+console.log(typeof (password)); //checks the data type of the input
 
-let firstCharacter = console.log(password.charAt(0)); //logs + charAt(0) identifies the first character in the variable 'password'.
+let firstCharacter = console.log(password.charAt(0)); //logs + charAt(0) identifies the first character in the variable
 
-let validLength = console.log(password.length);
-//console.log(password.length); //logs + .length checks the number of charcters in the variable 'password'.
-
-function validPassword(password){
-    //validates first character is a letter
-    if (firstCharacter == [/a-z, A-Z/]) {
-    console.log(firstCharacter);
-} 
-else (firstCharacter !== [/a-z, A-Z/]); {
-    prompt("Password must start with a letter");
-},
-
-//validates password length
-if (password.length <= 6 || password.length >= 20) {
-    console.log(password.length);
-} 
-else (password.length < 6 || password.length > 20); {
-    prompt("Password is the incorrect number of characters.")
-};
-
-
-};
-
-
-
-password.
+console.log(password.length);
+//console.log(password.length); //logs + .length checks the number of charcters in the variable
 
 //Validates first character is a letter
 
-//if (firstCharacter == [/a-z, A-Z/]) {
-    //console.log(firstCharacter);
-//} 
-//else (firstCharacter !== [/a-z, A-Z/]); {
-    //prompt("Password must start with a letter")
-//};
+if (firstCharacter == [/a-z, A-Z/]) {
+    console.log(firstCharacter);
+} else (firstCharacter !== [/a-z, A-Z/]); {
+    prompt("Password must start with a letter")
+};
 
 
 
 //Validates Password Minimum and Maximum Length
 
-//if (password.length <= 6 || password.length >= 20) {
-    //console.log(password.length);
-//} 
-//else (password.length < 6 || password.length > 20); {
-    //prompt("Password is the incorrect number of characters.")
-//};
+if (password.length <= 6 || password.length >= 20) {
+    console.log(password.length);
+} else (password.length < 6 || password.length > 20); {
+    prompt("Password is the incorrect number of characters.")
+};
 
 
-//if ((firstCharacter && password.length)){
-    //alert ("Your password has been accepted!");
-//}
-//else (prompt ("Your password is incorrect" ));
+if ((firstCharacter && password.length)){
+    alert ("Your password has been accepted!");
+}
+else (prompt ("Your password is incorrect" ));
