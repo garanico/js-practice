05e0973cf58a212b3603b03942cf8b83701585b2
Grace Aranico <grace.aranico@gmail.com>
2021-07-16 20:57:31 -0700

    consolidated Day3 files

---
+++ Day-6/js/study-group.js
@@ -0,0 +1,31 @@
+const url = async(pokename)=>{
+    // this is the API URL
+    const apiUrl = "https://pokeapi.co/api/v2/";
+    // this is the end point
+    const endPoint = `pokemon/${pokename}`; 
+    //this ask to find the data
+    const pokeData = await fetch(apiUrl + endPoint)
+    //if you find the API...
+    .then(res => {
+        //...returning the data and converting to json format...
+        return res.json();
+    }).then(data => {
+        //..return the data
+        return data;
+    })
+//logs 
+    console.log(pokeData);
+    //alert the user the name of the pokemon
+    alert(pokeData.name)
+
+    return pokeData;
+}
+//calling the function
+url("cubone");
+
+
+
+
+
+
+
